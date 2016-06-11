f6b23912f6010303ac5602bd94988f2cbdff623e
EnriqueOnAir <rexasuc@gmail.com>
2016-06-11 21:15:45 +0300

    Update extension.js

--- extension.js
+++ extension.js
@@ -47,8 +47,8 @@
 
     localStorage.setItem("basicBotsettings", JSON.stringify({
       botName: "MCloud_Bot",
-      language: "english",
-      chatLink: "https://rawgit.com/basicBot/source/master/lang/en.json",
+      language: "lithuanian",
+      chatLink: "https://github.com/EnriqueOnAir/bscbot/blob/master/lang/lt.json",
       scriptLink: "https://rawgit.com/basicBot/source/master/basicBot.js",
       roomLock: false, // Requires an extension to re-load the script
       startupCap: 1, // 1-200

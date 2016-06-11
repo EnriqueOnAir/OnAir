bd279d95c11f7479673b7fe07cdc022603713217
EnriqueOnAir <rexasuc@gmail.com>
2016-06-11 23:01:10 +0300

    Update extension.js

--- extension.js
+++ extension.js
@@ -48,8 +48,8 @@
     localStorage.setItem("basicBotsettings", JSON.stringify({
       botName: "MCloud_Bot",
       language: "lithuanian",
-      chatLink: "https://rawgit.com/EnriqueOnAir/bscbot/master/lang/lt.json",
-      scriptLink: "https://rawgit.com/basicBot/source/master/basicBot.js",
+      chatLink: "https://rawgit.com/EnriqueOnAir/basicBot/master/lang/lt.json",
+      scriptLink: "https://rawgit.com/EnriqueOnAir/basicBot/master/basicBot.js",
       roomLock: false, // Requires an extension to re-load the script
       startupCap: 1, // 1-200
       startupVolume: 0, // 0-100
@@ -112,6 +112,6 @@
     }));
 
     // Start the bot and extend it when it has loaded.
-    $.getScript("https://rawgit.com/basicBot/source/master/basicBot.js", extend);
+    $.getScript("https://rawgit.com/EnriqueOnAir/basicBot/master/basicBot.js", extend);
 
 }).call(this);
